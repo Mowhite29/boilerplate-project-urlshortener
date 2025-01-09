804a867740ff20067fbe20df08c644f9455e1181
Moses White <mowhite29@hotmail.co.uk>
2025-01-09 14:04:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -15,6 +17,52 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
+mongoose.connect(process.env.MONGO_URI);
+
+const shortUrlSchema = new mongoose.Schema({
+  original_url: {type: String, required: true},
+  short_url: Number
+});
+
+const ShortUrl = mongoose.model('ShortUrl', shortUrlSchema);
+
+app.use("/", bodyParser.urlencoded({extended: false}))
+
+app.use("/api/shorturl/:url", (req, res) => {
+  var url = req.params.url;
+  ShortUrl.findOne({short_url: url}, {original_url: 1}).then(data => {
+    console.log("short url" + url + data.original_url)
+    res.redirect(data.original_url)
+  });
+});
+
+app.post("/api/shorturl/", (req, res) => {
+  var url = req.body.url;
+  console.log("POST url" + " " + url)
+  ShortUrl.findOne().sort({short_url: -1}).limit(1).then(data => {
+    const number = data.short_url + 1;
+    console.log(number)
+    let regex = /https:\/\//i;
+    if (regex.test(url)){
+        var newEntry = new ShortUrl({
+          original_url: url,
+          short_url: number
+        })
+        res.json({
+          original_url: url,
+          short_url: number
+        })
+        console.log("original_url:" + " " + url + " " + "short_url:" + " " + number)
+        newEntry.save()
+      }else {
+        res.json({
+          error: 'invalid url'
+        });
+      console.log("invalid url")
+      }
+    });
+});
+
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
